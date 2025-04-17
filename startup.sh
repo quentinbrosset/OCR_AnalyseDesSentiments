0a9e0c8cb7d98c76c32ce94ebdb414b4c7d26335
Quentin Brosset <160849619+quentinbrosset@users.noreply.github.com>
2025-04-17 16:46:19 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "Installation des dépendances..."
+pip install -r requirements_dev.txt
+python -m nltk.downloader punkt stopwords wordnet punkt_tab
+
+echo "Démarrage des applications..."
 cd Application
 uvicorn api:app --host 0.0.0.0 --port $PORT &
 streamlit run app.py --server.port 8501 --server.address 0.0.0.0
