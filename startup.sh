d92dcf7ff7a83cd0aee56a0bd8bb7223a2f9cd9d
quentinbrosset <brosset.quentin@gmail.com>
2025-04-24 10:04:54 +0200

    Modif des fichier .deployment et startup

--- startup.sh
+++ startup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo "Installation des dépendances..."
-pip install -r requirements_dev.txt
-python -m nltk.downloader punkt stopwords wordnet punkt_tab
+export PATH="$HOME/python/bin:$PATH"
+
+# Utiliser le Python d'Azure App Service
+/home/site/wwwroot/env/bin/pip install -r requirements_dev.txt
+/home/site/wwwroot/env/bin/python -m nltk.downloader punkt stopwords wordnet punkt_tab
 
 echo "Démarrage des applications..."
 cd Application
-uvicorn api:app --host 0.0.0.0 --port $PORT &
-streamlit run app.py --server.port 8501 --server.address 0.0.0.0
+/home/site/wwwroot/env/bin/uvicorn api:app --host 0.0.0.0 --port $PORT &
+/home/site/wwwroot/env/bin/streamlit run app.py --server.port 8501 --server.address 0.0.0.0
